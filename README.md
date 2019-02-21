e7c50c2c7aea88f8b924e7238408cf8216ecd8c2
justinn <mintzer.justin@gmail.com>
2019-02-21 14:25:18 -0500

    Set up basic server that listens on port 8080 using morgan as HTTP logging middleware

---
+++ README.md
@@ -0,0 +1,14 @@
+## GarbGuess
+
+Ever had a hard time deciding what to wear and when? Keeping track of your clothes and esuring that eacho f your outfits get plenty of wear time? INtroducing GarbGuess, the next-gen application to help you decide what to wear for different occasions, weather, or daily. Is it raining? Take warmer clothing and a jacket. Job interview coming up? GarbGuess will help guess your next job-winning combination.
+
+## How to run
+In the project directory, you can run:
+
+### `npm dev-start`
+
+Runs the application in development mode - It will build the client and update in real-time.
+
+### `npm start`
+
+Runs the application in production mode - Will not build the client.
---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+
+//404 Error handling
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error('Not found');
+    err.status = 404;
+    next(err);
+  } else {
+    next();
+  }
+});
+
+//Everything's OK
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+});
+
+//Something went wrong if we got to here
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+//Start listening
+app.listen(8080, () => console.log(`Server listening on port 8080`));
